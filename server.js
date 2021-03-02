5901c5a47ce70bbaa567da7a1e55b4ad22c22654
Nina Jade <jaderiver64@gmail.com>
2021-03-02 00:16:31 -0600

    server working, heroku set-up and filterByQuery working

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const { notes } = require('./Develop/db/db');
 
 const app = express();
 
+function filterByQuery(query, notesArray) {
+
+    let filteredResults = notesArray;
+
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if (query.id) {
+        filteredResults = filteredResults.filter(note => note.id === query.id);
+    }
+    return filteredResults;
+
+}
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+
+    }
     res.json(results);
 });
 
