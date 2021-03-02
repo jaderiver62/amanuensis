52f3ac65f004c3aa4bb68532f6f32e8141a580ad
Nina Jade <jaderiver64@gmail.com>
2021-03-01 23:20:26 -0600

    server set-up

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const app = require('express');
+const express = require('express');
+const { notes } = require('./Develop/db/db');
+
+
+const app = express();
+
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
