db5252a885c3eb652a56e7369bcaa8a21eb3a555
Nina Cummings <jaderiver64@gmail.com>
2021-03-06 21:11:13 -0600

    routing updates

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const shortid = require('shortid');
 const { filterByQuery,
 	findById,
 	createNewNote,
 	deleteNote } = require('../../lib/notes');
 const { notes } = require('../../db/note.json')
 
-app.get('/api/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
@@ -13,7 +14,7 @@ app.get('/api/notes', (req, res) => {
     }
     res.json(results);j
 });
-app.get('/api/notes/:id', (req, res) => {
+router.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
         res.json(result);
@@ -21,12 +22,12 @@ app.get('/api/notes/:id', (req, res) => {
         res.send(404);
     }
 });
-app.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     req.body.id = shortid.generate();
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
-app.delete('/api/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res) => {
     const note = deleteNote(req.params.id, notes);
     res.json(note);
 });
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const { notes } = require('./db/db.json');
+
 
 const express = require('express');
-const shortid = require('shortid');
+
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
@@ -18,74 +16,74 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-function filterByQuery(query, notesArray) {
+// function filterByQuery(query, notesArray) {
 
-    let filteredResults = notesArray;
+//     let filteredResults = notesArray;
 
-    if (query.title) {
-        filteredResults = filteredResults.filter(note => note.title === query.title);
-    }
-    if (query.text) {
-        filteredResults = filteredResults.filter(note => note.text === query.text);
-    }
+//     if (query.title) {
+//         filteredResults = filteredResults.filter(note => note.title === query.title);
+//     }
+//     if (query.text) {
+//         filteredResults = filteredResults.filter(note => note.text === query.text);
+//     }
 
-    return filteredResults;
+//     return filteredResults;
 
-}
+// }
 
-function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-}
+// function findById(id, notesArray) {
+//     const result = notesArray.filter(note => note.id === id)[0];
+//     return result;
+// }
 
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-}
+// function createNewNote(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//         JSON.stringify({ notes: notesArray }, null, 2)
+//     );
+//     return note;
+// }
 
-function deleteNote(id, notesArray) {
-    const newNotesArray = notesArray.filter(note => note.id !== id);
-    const removed = findById(id, notesArray);
+// function deleteNote(id, notesArray) {
+//     const newNotesArray = notesArray.filter(note => note.id !== id);
+//     const removed = findById(id, notesArray);
 
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: newNotesArray }, null, 2)
-    );
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//         JSON.stringify({ notes: newNotesArray }, null, 2)
+//     );
 
-    return newNotesArray;
-}
+//     return newNotesArray;
+// }
 
 
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
+// app.get('/api/notes', (req, res) => {
+//     let results = notes;
+//     if (req.query) {
+//         results = filterByQuery(req.query, results);
 
-    }
-    res.json(results);
-});
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-app.post('/api/notes', (req, res) => {
-    req.body.id = shortid.generate();
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-});
-app.delete('/api/notes/:id', (req, res) => {
-    const note = deleteNote(req.params.id, notes);
-    res.json(note);
-});
+//     }
+//     res.json(results);
+// });
+// app.get('/api/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, notes);
+//     if (result) {
+//         res.json(result);
+//     } else {
+//         res.send(404);
+//     }
+// });
+// app.post('/api/notes', (req, res) => {
+//     req.body.id = shortid.generate();
+//     const note = createNewNote(req.body, notes);
+//     res.json(note);
+// });
+// app.delete('/api/notes/:id', (req, res) => {
+//     const note = deleteNote(req.params.id, notes);
+//     res.json(note);
+// });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
